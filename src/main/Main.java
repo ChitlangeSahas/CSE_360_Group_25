3ab96f74e727d3713b7040b397d21fce4741b1c3
chitlangesahas <chitlangesahas@gmail.com>
2019-11-23 21:22:04 -0700

    changed design layout for the screens, focusing on the GUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +15,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("data_screen.fxml"));
         primaryStage.setTitle("Grade Analytics Tool");
-        primaryStage.setScene(new Scene(root, 800, 500));
+        Scene scene = new Scene(root, 600, 352);
+        primaryStage.setScene(scene);
+
+
         primaryStage.show();
     }
 
