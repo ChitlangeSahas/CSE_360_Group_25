b70f1cea81dca40ab920f3f4dfeda56de997dc18
chitlangesahas <chitlangesahas@gmail.com>
2019-11-29 15:27:31 -0700

    added one click listener to entry screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,21 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("settings_screen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("entry_screen.fxml"));
         primaryStage.setTitle("Grade Analytics Tool");
         Scene scene = new Scene(root, 600, 352);
         primaryStage.setScene(scene);
 
-
         primaryStage.show();
     }
-
 
 
     public static void main(String[] args) {
